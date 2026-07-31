07552da2603b7cd4bd2cc78ecbb41ea47a2b71f0
thenameisshreesh <shreeshpitambare084@gmail.com>
2026-07-31 12:03:41 +0530

    Time: 20 ms (83.29%), Space: 56.4 MB (57.59%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        int l=0,r=people.length-1,boatCount=0;
+
+        Arrays.sort(people);
+
+        while (l<=r) {
+
+            if(people[l]+people[r]<=limit)
+            {
+
+                l++;
+                r--;
+                boatCount++;
+
+            }
+            else
+            {
+                boatCount++;
+                r--;
+                
+            }
+
+            
+        }
+
+        return boatCount;
+
+    }
+
+}
