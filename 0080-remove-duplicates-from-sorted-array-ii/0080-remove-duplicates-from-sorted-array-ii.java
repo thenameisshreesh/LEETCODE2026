26e6bf81455fbb9286187669c0f87070c5bf62c5
thenameisshreesh <shreeshpitambare084@gmail.com>
2026-08-05 10:05:48 +0530

    Time: 0 ms (100%), Space: 48.9 MB (32.6%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,17 +1,40 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length <= 2) {
-            return nums.length;
-        }
+
+    int slow = 0, fast = 0;
         
-        int k = 2; // Start writing from index 2
-        for (int i = 2; i < nums.length; i++) {
-            // Check if the current element is different from nums[k-2]
-            if (nums[i] != nums[k - 2]) {
-                nums[k] = nums[i];
-                k++;
+
+    while (fast < nums.length) {
+
+        if(slow>=2)
+        {
+                
+            if(nums[fast]==nums[slow-2])
+            {
+                
+                   
+                fast++;
+                continue;
+
             }
+
+            
+
+                
+
         }
-        return k;
+        nums[slow]=nums[fast];
+        slow++;
+        fast++;
+        
+
+           
+
     }
+
+        
+
+    return slow;
+
+}
 }
